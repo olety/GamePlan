f5979970ca4eea6e47160af26ecb5e4ac9a3300b
PaulinaBinas <binaspaulina1@gmail.com>
2017-11-30 19:17:35 +0100

    Basic setup of chat and shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { ChatComponent } from './chat/chat.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
