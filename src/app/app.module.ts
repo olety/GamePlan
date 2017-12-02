9149afcf3942d4cdab63f55df5cee78732832dd7
Oleksii Kyrylchuk <olety7@gmail.com>
2017-12-02 11:44:49 +0100

    Materialize should be working now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { enableProdMode } from '@angular/core';
+import { MaterializeModule } from 'angular2-materialize';
 
 
 import { AppComponent } from './app.component';
