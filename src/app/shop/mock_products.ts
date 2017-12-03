a975f1323d20b9e1fe182d94e494af70348dd66e
PaulinaBinas <binaspaulina1@gmail.com>
2017-12-03 15:04:50 +0100

    Added emoji to the shop

--- src/app/shop/mock_products.ts
+++ src/app/shop/mock_products.ts
@@ -1,13 +1,15 @@
 import { Product } from './Product';
 
 export const PRODUCTS: Product[] = [
-    { price: 10, imagesrc: 'url(../../assets/tiles-pattern.jpg)' },
+    { price: 10, imagesrc: 'url(../../assets/polka-pattern.jpg)' },
     { price: 10, imagesrc: 'url(../../assets/checked-pattern)' },
     { price: 15, imagesrc: 'url(../../assets/swirl-pattern.jpg)' },
     { price: 15, imagesrc: 'url(../../assets/waves-pattern.jpg)' }
 ];
 
 export const PRODUCTS2: Product[] = [
-    { price: 5, imagesrc: 'url(../../assets/smile.svg)' },
-    { price: 5, imagesrc: 'url(../../assets/smile.svg)' }
+    { price: 5, imagesrc: 'url(../../assets/008-happy.svg)' },
+    { price: 5, imagesrc: 'url(../../assets/003-laughing-1.svg)' },
+    { price: 5, imagesrc: 'url(../../assets/002-cool.svg)' },
+    { price: 5, imagesrc: 'url(../../assets/001-vain.svg)' }
 ];
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product }           from './Product';
 import { PRODUCTS }          from './mock_products';
+import { PRODUCTS2 }         from './mock_products';
 
 @Component({
   selector: 'app-shop',
@@ -9,7 +10,8 @@ import { PRODUCTS }          from './mock_products';
 })
 export class ShopComponent implements OnInit {
 
-  products: Product[] = PRODUCTS;
+    products: Product[] = PRODUCTS;
+    products2: Product[] = PRODUCTS2;
 
   constructor() { }
 
