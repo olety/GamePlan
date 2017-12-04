932d132ef2ef2d6bc52af5fcf20e8d77dbb7ee43
PaulinaBinas <binaspaulina1@gmail.com>
2017-12-04 13:09:38 +0100

    Fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { enableProdMode } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {NgxPaginationModule} from 'ngx-pagination';
-import { MaterializeModule } from 'angular2-materialize';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'angular-calendar';
+import { NgxPaginationModule }  from 'ngx-pagination';
+import { MaterializeModule } from 'angular2-materialize';
 
-
-=======
 import { DragulaModule } from 'ng2-dragula';
->>>>>>> Pushing the day/task views
 import { AppComponent } from './app.component';
 import { DayComponent } from './day/day.component';
 import { ShopComponent } from './shop/shop.component';
 import { ChatComponent } from './chat/chat.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { TaskComponent } from './task/task.component';
 
@@ -39,14 +35,11 @@ import { NgDatepickerModule } from 'ng2-datepicker';
         BrowserModule,
         AppRoutingModule,
         FormsModule,
-<<<<<<< HEAD
         NgxPaginationModule,
         BrowserAnimationsModule,
-        CalendarModule.forRoot()
-=======
+        CalendarModule.forRoot(),
         DragulaModule,
         NgDatepickerModule
->>>>>>> Pushing the day/task views
     ],
     providers: [],
     bootstrap: [AppComponent]
