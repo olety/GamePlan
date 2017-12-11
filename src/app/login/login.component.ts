485796ae6b4a54127f703e4e6675f8ce4b9c7840
Oleksii Kyrylchuk <olety7@gmail.com>
2017-12-11 09:56:16 +0100

    Fixed the /login navbar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+        document.getElementsByClassName('menu')[0].style.visibility = 'hidden';
+        document.getElementsByClassName('signin')[0].style.visibility = 'hidden';
+        document.getElementsByTagName('nav')[0].style.visibility = 'hidden';
+  }
 
   ngOnInit() {
   }
